c31a5f2b24b11accf5e6c867ee334f8bc98f8e48
RafaelDoering <rafael.doering.soares@usp.br>
2020-03-02 23:51:18 -0300

    favicons

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,14 +5,36 @@ export default {
   ** Headers of the page
   */
   head: {
-    title: process.env.npm_package_name || '',
+    title: 'Semcomp',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      {
+        hid: 'description',
+        name: 'description',
+        content:
+          'A melhor semana acadêmica de computação do Brasil'
+      },
+      {
+        property: 'og:title',
+        content: 'Semcomp'
+      },
+      {
+        property: 'og:description',
+        content:
+          'A melhor semana acadêmica de computação do Brasil'
+      },
+      { name: 'apple-mobile-web-app-title', content: 'Semcomp' },
+      { name: 'application-name', content: 'Semcomp' },
+      { name: 'msapplication-TileColor', content: '#101010' },
+      { name: 'theme-color', content: '#101010' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon/favicon.ico' },
+      { rel: 'apple-touch-icon', sizes: '180x180', href: '/favicon/apple-touch-icon.png' },
+      { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon/favicon-32x32.png' },
+      { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon/favicon-16x16.png' },
+      { rel: 'mask-icon', href: '/favicon/safari-pinned-tab.svg', color: '#101010' }
     ]
   },
   /*
